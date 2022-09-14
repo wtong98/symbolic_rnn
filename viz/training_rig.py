58a104ea682b43dfb5d002ba9340628efdd4ea32
William Tong <williamtong105@gmail.com>
2022-09-14 15:43:04 -0400

    Polish changes for presentation

--- viz/training_rig.py
+++ viz/training_rig.py
@@ -8,20 +8,31 @@ from collections import namedtuple
 import matplotlib.pyplot as plt
 import numpy as np
 
+from torch.utils.data import ConcatDataset
+
 import sys
 sys.path.append('../')
 
 from model import *
 
-def run_case(model_params, ds_params_set, n_epochs=1000, n_zeros=20):
+def run_case(model_params, ds_params_set, n_epochs=1000, n_zeros=20, interleaved=False):
     model = RnnClassifier(**model_params).cuda()
     results = []
 
-    for params in ds_params_set:
-        ds = BinaryAdditionDataset(**params)
+    if interleaved:
+        all_ds = [BinaryAdditionDataset(**params) for params in ds_params_set]
+        ds = ConcatDataset(all_ds)
+        ds.pad_collate = all_ds[0].pad_collate
+
         train_dl = DataLoader(ds, batch_size=32, shuffle=True, collate_fn=ds.pad_collate, num_workers=0, pin_memory=True)
         test_dl = DataLoader(ds, batch_size=32, collate_fn=ds.pad_collate, num_workers=0, pin_memory=True)
         model.learn(n_epochs, train_dl, test_dl, lr=5e-5, eval_every=100)
+    else:
+        for params in ds_params_set:
+            ds = BinaryAdditionDataset(**params)
+            train_dl = DataLoader(ds, batch_size=32, shuffle=True, collate_fn=ds.pad_collate, num_workers=0, pin_memory=True)
+            test_dl = DataLoader(ds, batch_size=32, collate_fn=ds.pad_collate, num_workers=0, pin_memory=True)
+            model.learn(n_epochs, train_dl, test_dl, lr=5e-5, eval_every=100)
 
     print('testing')
     with torch.no_grad():
@@ -34,7 +45,7 @@ def run_case(model_params, ds_params_set, n_epochs=1000, n_zeros=20):
     print('done')
     return results
 
-Case = namedtuple('Case', 'name model_params ds_params_set results')
+Case = namedtuple('Case', 'name model_params ds_params_set is_interleaved results', defaults=[False, []])
 
 common_model_args = {
     'max_arg': 0,
@@ -79,24 +90,33 @@ case_set = [
             dict({'n_bits': 3, 'max_args': 3}, **common_ds_args),
             dict({'n_bits': 7, 'max_args': 1}, **common_ds_args)
          ], results=[]),
+    Case(name='Interleaved', 
+         model_params=dict({'nonlinearity': 'relu'}, **common_model_args, ),
+         ds_params_set=[
+            dict({'n_bits': 3, 'max_args': 3}, **common_ds_args),
+            dict({'n_bits': 7, 'max_args': 1}, **common_ds_args)
+         ], is_interleaved=True),
 ]
 
 for c in case_set:
-    results = run_case(c.model_params, c.ds_params_set)
+    results = run_case(c.model_params, c.ds_params_set, interleaved=c.is_interleaved)
     c.results.extend(results)
 
 # %%
 xs = np.arange(21)
 ys = 2 ** xs
 
+plt.gcf().set_size_inches(8, 6)
+
 plt.plot(xs, ys, 'o--', color='black', label='True')
 plt.xticks(xs[::2])
 plt.axvline(x=7, color='red')
+plt.axvline(x=2, color='magenta')
 # plt.annotate('train', (5, 2))
 # plt.annotate('test', (7.5, 2))
 
 for c in case_set:
-    plt.plot(xs, c.results, 'o--', label=c.name, alpha=0.7)
+    plt.plot(xs, c.results, 'o--', label=c.name, alpha=0.6)
 
 plt.yscale('log', base=2)
 plt.legend()
